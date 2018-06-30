522e85f4d6f9af87fff18ddbb5767edb15b2ddb6
Paul den Dulk <pauldendulk@gmail.com>
2018-06-30 11:15:22 +0200

    Full project

---
+++ CrashWhenDrawingPolygon/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UIKit;
+
+namespace CrashWhenDrawingPolygon
+{
+    public partial class ViewController : UIViewController
+    {
+		public ViewController (IntPtr handle) : base (handle)
+		{
+            
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+		    View = new MyView(View.Bounds);
+        }
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+    }
+}
